95c93cf18933bfa129d460ce1737c9c8cb58e040
hqz3 <heqzheng@gmail.com>
2023-02-03 00:03:49 -0500

    Create login route and createSessionHandler controller

---
+++ start/api/controllers/controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getUser } from "../src/db";
+import { signJWT, verifyJWT } from "../utils/jwt";
+
+export function createSessionHandler(req: Request, res: Response) {
+  const { email, password } = req.body;
+  const user = getUser(email);
+
+  if (!user || user.password !== password) {
+    return res.status(401).send("Invalid email or password");
+  }
+
+  // // create access token
+  const accessToken = signJWT({ email: user.email, name: user.name }, "1h");
+  console.log(accessToken);
+
+  // set access token in cookie
+  res.cookie("accessToken", accessToken, { maxAge: 300000, httpOnly: true });
+
+  // send user back
+  res.send(verifyJWT(accessToken).payload);
+}
--- start/api/src/routes.ts
+++ start/api/src/routes.ts
@@ -1,5 +1,12 @@
 import { Express } from "express";
+import { createSessionHandler } from "../controllers/controller";
 
-function routes(app: Express) {}
+function routes(app: Express) {
+  // login
+  app.post("/api/session", createSessionHandler);
+
+  // get the current session
+  // logout
+}
 
 export default routes;
