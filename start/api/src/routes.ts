52ea7e373f76f2ea022027dc9697bfa2db6df86d
hqz3 <heqzheng@gmail.com>
2023-02-03 12:24:31 -0500

    Implement getSession and deleteSession routes

--- start/api/src/routes.ts
+++ start/api/src/routes.ts
@@ -5,15 +5,17 @@ import {
   deleteSessionHandler,
 } from "../controllers/controller";
 
+import requireUser from "../middleware/requireUser";
+
 function routes(app: Express) {
   // login
   app.post("/api/session", createSessionHandler);
 
   // get the current session
-  app.get("/api/session", getSessionHandler);
+  app.get("/api/session", requireUser, getSessionHandler);
 
   // logout
-  app.delete("/api/session", deleteSessionHandler);
+  app.delete("/api/session", requireUser, deleteSessionHandler);
 }
 
 export default routes;
